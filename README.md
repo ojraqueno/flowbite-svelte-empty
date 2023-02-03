d920a226107d291781a5034cdb62c0c5f71baeb5
OJ <oj.raqueno@gmail.com>
2023-02-03 20:20:38 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# flowbite-svelte-empty
+
+SvelteKit with FlowBite starter project with all the latest npm package versions as of Feb. 3, 2023.
+
+Created using:
+- node 18.12.1
+- npm 8.19.2
+- svelte 3.55.1
+- sveltekit 1.3.10
+- tailwindcss 3.2.4
+- flowbite-svelte 0.29.13
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { Button } from 'flowbite-svelte';
+</script>
+
+<section class="bg-white dark:bg-gray-900 min-h-screen">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+        <Button>Default</Button>
+        <Button color="alternative">Alternative</Button>
+        <Button color="dark">Dark</Button>
+        <Button color="light">Light</Button>
+        <Button color="green">Green</Button>
+        <Button color="red">Red</Button>
+        <Button color="yellow">Yellow</Button>
+        <Button color="purple">Purple</Button>
+    </div>
+</section>
